9a1dc6377ed6f393e12e77fe32e9fb5d5e34e502
khaledkee <khaledkee0@gmail.com>
2016-08-02 18:52:32 +0200

    Add jdbc Add getName

--- src/main/java/edu/orange/internship/User.java
+++ src/main/java/edu/orange/internship/User.java
@@ -1,13 +1,41 @@
 package edu.orange.internship;
 
+import java.sql.*;
+
 /**
  * Created by Mustafa on 8/2/2016.
  */
 public class User {
     private  String name;
+
     public String getName() {
         return  name;
     }
+
+    public Boolean findName(String root, String password, String username) throws SQLException {
+        Boolean result = false;
+        final String connectionString = "jdbc:mysql://localhost:3306/urldata";
+        final String checkStatement = "SELECT * FROM user WHERE name = ?";
+        Connection connection = null;
+        PreparedStatement checkPreparedStatement = null;
+        try{
+            connection = DriverManager.getConnection(connectionString,root,password);
+            checkPreparedStatement = connection.prepareStatement(checkStatement);
+            checkPreparedStatement.setString(1,username);
+            ResultSet foundUsers = checkPreparedStatement.executeQuery();
+            result = foundUsers.next();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            result = false;
+        }finally {
+            if(checkPreparedStatement != null)
+                checkPreparedStatement.close();
+            if(connection != null)
+                connection.close();
+        }
+        return result;
+    }
+
     public void setName(String newName){
         name=newName;
     }
--- src/test/java/edu/orange/internship/UserTest.java
+++ src/test/java/edu/orange/internship/UserTest.java
@@ -1,8 +1,37 @@
 package edu.orange.internship;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
 /**
  * Created by Mustafa on 8/2/2016.
  */
 public class UserTest {
+    @Test
+    public void findExistNameTest() throws Exception{
+        String usernames [] = {"rem","mustafa","kee"};
+        final String root = "root",password = "13KNWcoding";
+        for(String username:usernames){
+            Boolean result = new User().findName(root, password, username);
+            assertTrue(username,result);
+        }
+    }
 
+    @Test
+    public void findNonExistNameTest() throws Exception{
+        String usernames [] = {"zetamoo","tsunami","moha"};
+        final String root = "root",password = "13KNWcoding";
+        for(String username:usernames){
+            Boolean result = new User().findName(root, password, username);
+            assertFalse(username,result);
+        }
+    }
+
+    @Test
+    public void setExistentUserTest() throws Exception{
+
+    }
 }
